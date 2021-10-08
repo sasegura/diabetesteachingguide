a9930f7998e9f51d14a2108459ec42c79b3816f8
sasegura <sasegura.fernandez87@gmail.com>
2021-10-08 13:27:16 +0200

    Organizando el componente ForumChat

---
+++ src/views/Sections/ForumSection.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { withTranslation } from "react-i18next";
+import { PropTypes } from "prop-types";
+import Chat from "components/Chat/Chat";
+
+ForumSection.propTypes = {
+  t: PropTypes.func
+};
+
+function ForumSection() {
+  return <Chat collection={"messages"} />;
+}
+export default withTranslation("translations")(ForumSection);
