85f488d1e77a97e54ecc66a7334667e37cec3477
vsegura89 <48039393+vsegura89@users.noreply.github.com>
2021-10-21 12:08:37 -0400

    Organizando el contenido de forma general (#51)
    * Organizando el contenido
    
    * Correccion de dependencias en "package.json".
    Agregado "package-lock.json" al ".gitignore".
    Corregido error al importar modulo "firebase" en el fichero ForumSection" probablemente dado por las diferncias de versiones en los paquetes.
    
    * Organizando y puliendo todo el contenido
    
    * Organizando y puliendo ficheros de traduccion

--- src/views/Sections/ForumSection.js
+++ src/views/Sections/ForumSection.js
@@ -18,10 +18,11 @@ import { CircularProgress } from "@material-ui/core";
 
 import { makeStyles } from "@material-ui/core/styles";
 
-import firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
-import "firebase/analytics";
+// import "firebase/app";
+// import "firebase/firestore";
+// import "firebase/auth";
+// import "firebase/analytics";
+import firebase from "firebase/compat";
 
 firebase.initializeApp({
   apiKey: process.env.REACT_APP_AUTH_API_KEY,
